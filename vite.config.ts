b49765f51a777733cd5e22e4d899ee3f847f4e9e
ApollonDeParnasse <echase3@gmail.com>
2026-06-15 10:04:49 -0500

    feat: basic https setup for network testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import { patchCssModules } from "vite-css-modules";
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   base: "/org-everywhere/",
@@ -11,11 +12,14 @@ export default defineConfig({
     nodePolyfills(),
     githubPagesSpa({ verbose: true }),
     patchCssModules(),
+    mkcert({
+      savePath: "./cert"
+    })
   ],
   build: {
     rollupOptions: {
       external: ["**/*.{unit, browser, integration}.test.ts[x]"],
     },
     target: "es2024",
-  },
+  },  
 });
